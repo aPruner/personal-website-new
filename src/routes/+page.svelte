65d5c5afced72dd8b60b2971d051483e2834a9e1
aPruner <adampruner99@gmail.com>
2023-07-16 11:47:41 -0700

    update text for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,10 @@
   <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
       <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-teal-300 md:text-5xl lg:text-6xl">Adam Pruner</h1>
-      <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">I'm a software generalist who likes to build all sorts of things, including games</p>
-      <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here's some other text that has a little bit longer of a length than the above text</p>
+      <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+        I'm a software generalist who specializes in full stack web applications and likes to build games for fun. I'm currently contributing my expertise to the cyber insurance industry at 
+        <a href="https://coalitioninc.com" class="text-blue-600">Coalition</a>.
+      </p>
     </div>
   </section>
 </div>
