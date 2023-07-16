87218832447916f4144501b88552eb415dddad2a
aPruner <adampruner99@gmail.com>
2023-07-16 11:10:57 -0700

    fix some formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,9 @@
 <div>
   <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-teal-300 md:text-5xl lg:text-6xl">Adam Pruner</h1>
-        <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">I build software of all shapes, sizes, and functionalities.</p>
-        <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here's some other text that has a little bit longer of a length than the above text</p>
-
+      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-teal-300 md:text-5xl lg:text-6xl">Adam Pruner</h1>
+      <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">I'm a software generalist who likes to build all sorts of things, including games</p>
+      <p class="mb-4 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here's some other text that has a little bit longer of a length than the above text</p>
     </div>
   </section>
 </div>
