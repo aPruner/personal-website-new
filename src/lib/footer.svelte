342c51ea42f87ecd0c316cd424edcdae6fd955c2
aPruner <adampruner99@gmail.com>
2023-07-16 12:47:15 -0700

    fix up the background color in the root html

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -5,7 +5,7 @@
 <footer class="bg-slate-800">
   <div class="mx-auto px-2 sm:px-6 lg:px-8">
     <div class="flex">
-      <p class="p-2 text-white">Made with love, Svelte, and TailwindCSS</p>
+      <p class="p-6 text-white">Made with love, Svelte, and TailwindCSS</p>
     </div>
   </div>
 </footer>
